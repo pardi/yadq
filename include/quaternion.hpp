ca2ad1c546770afa01037690809415184bfa8e25
Pardi Tommaso <tommaso.pardi@gmail.com>
2023-06-27 23:38:32 +0100

    feat(quat): removed/added fcns
    - Removed the mod() function because of undefined behaviour of the operator
    - Simplified the function to convert a quaternion to a rotation and changed name to `quatToRotation`

--- include/quaternion.hpp
+++ include/quaternion.hpp
@@ -137,7 +137,7 @@ namespace yadq{
     template<   template<typename> class Base, 
                 typename T, 
                 typename =  std::enable_if_t<std::is_base_of_v<quaternion<T>, Base<T>>>>
-    constexpr inline auto operator-(const Base<T>& q_lhv, const Base<T>& q_rhv){
+    constexpr inline auto operator-(const Base<T>& q_lhv, const Base<T>& q_rhv) noexcept{
 
         Base<T> q_res(  q_lhv.w() - q_rhv.w(),
                         q_lhv.x() - q_rhv.x(),
@@ -154,7 +154,7 @@ namespace yadq{
     template<   template<typename> class Base, 
                 typename T, 
                 typename =  std::enable_if_t<std::is_base_of_v<quaternion<T>, Base<T>>>>
-    constexpr inline auto operator+(const Base<T>& q_lhv, const Base<T>& q_rhv){
+    constexpr inline auto operator+(const Base<T>& q_lhv, const Base<T>& q_rhv) noexcept{
 
         Base<T> q_res(  q_lhv.w() + q_rhv.w(),
                         q_lhv.x() + q_rhv.x(),
@@ -171,7 +171,7 @@ namespace yadq{
     template<   template<typename> class Base, 
                 typename T, 
                 typename =  std::enable_if_t<std::is_base_of_v<quaternion<T>, Base<T>>>>
-    constexpr inline auto operator+(const Base<T>& q_lhv, double rhv){
+    constexpr inline auto operator+(const Base<T>& q_lhv, double rhv) noexcept{
 
         Base<T> q_res(  q_lhv.w() + rhv,
                         q_lhv.x() + rhv,
@@ -188,14 +188,14 @@ namespace yadq{
     template<   template<typename> class Base, 
                 typename T, 
                 typename =  std::enable_if_t<std::is_base_of_v<quaternion<T>, Base<T>>>>
-    constexpr inline auto operator+(double lhv, const Base<T>& q_rhv){
+    constexpr inline auto operator+(double lhv, const Base<T>& q_rhv) noexcept{
         return q_rhv + lhv;
     }  
 
     template<   template<typename> class Base, 
                 typename T, 
                 typename =  std::enable_if_t<std::is_base_of_v<quaternion<T>, Base<T>>>>
-    constexpr auto operator*(double val, const Base<T>& q_rhv){ 
+    constexpr auto operator*(double val, const Base<T>& q_rhv) noexcept{ 
 
         Base<T> q_res(  q_rhv.w() * val,
                         q_rhv.x() + val,
@@ -208,14 +208,14 @@ namespace yadq{
     template<   template<typename> class Base, 
                 typename T, 
                 typename =  std::enable_if_t<std::is_base_of_v<quaternion<T>, Base<T>>>>
-    constexpr auto operator*(const Base<T>& q_lhv, double val){ 
+    constexpr auto operator*(const Base<T>& q_lhv, double val) noexcept{ 
         return val * q_lhv;
     }
 
     template<   template<typename> class Base, 
                 typename T, 
                 typename =  std::enable_if_t<std::is_base_of_v<quaternion<T>, Base<T>>>>
-    constexpr auto operator*(const Base<T>& q_lhv, const Base<T>& q_rhv) {
+    constexpr auto operator*(const Base<T>& q_lhv, const Base<T>& q_rhv) noexcept {
         return hamilton_prod(q_lhv, q_rhv);
     }
 
@@ -226,7 +226,7 @@ namespace yadq{
     template<   template <typename> class Base, 
                 typename T, 
                 typename =  std::enable_if_t<std::is_base_of_v<quaternion<T>, Base<T>>>>
-    constexpr Base<T> normalise(const Base<T>& q_in){
+    constexpr Base<T> normalise(const Base<T>& q_in) noexcept{
 
         if(auto d = q_in.norm(); d != 0.0) {
 
@@ -242,27 +242,14 @@ namespace yadq{
     }
 
     template<typename T>
-    std::ostream& operator<<(std::ostream &os, const quaternion<T>& q_in) { 
+    std::ostream& operator<<(std::ostream &os, const quaternion<T>& q_in) noexcept{ 
         return os << "w: " << q_in.w() << " x: " << q_in.x() << " y: " << q_in.y() << " z: " << q_in.z();
     }
 
     template<   template <typename> class Base, 
                 typename T, 
                 typename =  std::enable_if_t<std::is_base_of_v<quaternion<T>, Base<T>>>>
-    constexpr auto mod(const Base<T>& q_in){
-        
-        Base<T> q_res(  q_in.w() / fabs(q_in.w()),
-                        q_in.x() / fabs(q_in.x()),
-                        q_in.y() / fabs(q_in.y()),
-                        q_in.z() / fabs(q_in.z()));
-        
-        return q_res;
-    }
-
-    template<   template <typename> class Base, 
-                typename T, 
-                typename =  std::enable_if_t<std::is_base_of_v<quaternion<T>, Base<T>>>>
-    constexpr inline auto hamilton_prod(const Base<T>& q_lhv, const Base<T>& q_rhv) {
+    constexpr inline auto hamilton_prod(const Base<T>& q_lhv, const Base<T>& q_rhv) noexcept{
 
         Base<T> q_res(  q_lhv.w() * q_rhv.w() - q_lhv.x() * q_rhv.x() - q_lhv.y() * q_rhv.y() - q_lhv.z() * q_rhv.z(),
                         q_lhv.w() * q_rhv.x() + q_lhv.x() * q_rhv.w() + q_lhv.y() * q_rhv.z() - q_lhv.z() * q_rhv.y(),
@@ -291,7 +278,7 @@ namespace yadq{
     template<   template<typename> class Base, 
                 typename T, 
                 typename =  std::enable_if_t<std::is_base_of_v<quaternion<T>, Base<T>>>>
-    inline constexpr Base<T> conjugate(const Base<T>& q) {
+    inline constexpr Base<T> conjugate(const Base<T>& q) noexcept{
         Base<T> q_res(q);
         q_res.conjugate();
 
@@ -301,7 +288,7 @@ namespace yadq{
     template<   template<typename> class Base, 
                 typename T, 
                 typename =  std::enable_if_t<std::is_base_of_v<quaternion<T>, Base<T>>>>
-    inline constexpr Base<T> exp(const Base<T>& q_in) {
+    inline constexpr Base<T> exp(const Base<T>& q_in) noexcept{
         Base<T> q_conj(q_in);
         q_conj.conjugate();
 
@@ -315,7 +302,7 @@ namespace yadq{
     template<   template<typename> class Base, 
                 typename T, 
                 typename =  std::enable_if_t<std::is_base_of_v<quaternion<T>, Base<T>>>>
-    constexpr std::optional<Base<T>> acos(const Base<T>& q_in) {
+    constexpr std::optional<Base<T>> acos(const Base<T>& q_in) noexcept {
         
         if constexpr (q_in.empty()){
             return std::nullopt;
@@ -327,7 +314,7 @@ namespace yadq{
     template<   template<typename> class Base, 
                 typename T, 
                 typename =  std::enable_if_t<std::is_base_of_v<quaternion<T>, Base<T>>>>
-    constexpr std::optional<Base<T>>log(const Base<T>& q_in) {
+    constexpr std::optional<Base<T>>log(const Base<T>& q_in)  noexcept{
         
         Base<T> q_conj(q_in);
         q_conj.conjugate();
@@ -347,7 +334,7 @@ namespace yadq{
     template<   template<typename> class Base, 
                 typename T, 
                 typename =  std::enable_if_t<std::is_base_of_v<quaternion<T>, Base<T>>>>
-    constexpr Base<T> dot(const Base<T>& q_lhv, const Base<T>& q_rhv){
+    constexpr Base<T> dot(const Base<T>& q_lhv, const Base<T>& q_rhv) noexcept{
         return q_lhv.x() * q_rhv.x() + q_lhv.y() * q_rhv.y() + q_lhv.z() * q_rhv.z();
     }
 
@@ -355,7 +342,7 @@ namespace yadq{
     template<   template<typename> class Base, 
                 typename T, 
                 typename =  std::enable_if_t<std::is_base_of_v<quaternion<T>, Base<T>>>>
-    constexpr Base<T> interpolation(const Base<T>& q_start, const Base<T>& q_end, double t, InterpType interp_type = InterpType::LERP){
+    constexpr Base<T> interpolation(const Base<T>& q_start, const Base<T>& q_end, double t, InterpType interp_type = InterpType::LERP) noexcept{
         switch (interp_type)
         {
             case InterpType::LERP:
@@ -373,32 +360,25 @@ namespace yadq{
     template<   template<typename> class Base, 
                 typename T, 
                 typename =  std::enable_if_t<std::is_base_of_v<quaternion<T>, Base<T>>>>
-    constexpr auto toRotation(const Base<T>& q){
+    constexpr auto toRotation(const Base<T>& q) noexcept{
 
         auto a0 = pow(q.w(), 2);
         auto a1 = pow(q.x(), 2);
         auto a2 = pow(q.y(), 2);
         auto a3 = pow(q.z(), 2);
 
-        auto a4 = q.x() * q.w();
-        auto a5 = q.x() * q.y();
-        auto a6 = q.x() * q.z();
+        auto a4 = q.w() * q.x();
+        auto a5 = q.w() * q.y();
+        auto a6 = q.w() * q.z();
+
+        auto a7 = q.x() * q.y();
+        auto a8 = q.x() * q.z();
         
-        auto a7 = q.y() * q.w();
-        auto a8 = q.y() * q.x();
         auto a9 = q.y() * q.z();
 
-        auto a10 = q.z() * q.w();
-        auto a11 = q.z() * q.x();
-        auto a12 = q.z() * q.y();
-
-        auto a13 = q.w() * q.x();
-        auto a14 = q.w() * q.y();
-        auto a15 = q.w() * q.z();
-
-        std::array<T, 9> R = {  2 * (a0 + a1) - 1, 2 * (a5 - a15), 2 * (a6 + a14),
-                                2 * (a5 + a15), 2 * (a0 + a2) - 1, 2 * (a9 - a13),
-                                2 * (a6 - a14), 2 * (a9 + a13), 2 * (a0 + a4) - 1};
+        std::array<T, 9> R = {  2 * (a0 + a1) - 1, 2 * (a7 - a6), 2 * (a8 + a5),
+                                2 * (a7 + a6), 2 * (a0 + a2) - 1, 2 * (a9 - a4),
+                                2 * (a8 - a5), 2 * (a9 + a4), 2 * (a0 + a3) - 1};
 
         return R;
     }
