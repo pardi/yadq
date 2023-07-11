bd23dabf06588d76647250b1c006e8cd816587ba
Tommaso Pardi <tommaso.pardi@gmail.com>
2023-07-11 13:52:09 +0000

    feat(tests): added extra tests

--- tests/quaternion_test.cpp
+++ tests/quaternion_test.cpp
@@ -1,8 +1,9 @@
 #include <gtest/gtest.h>
 #include <quaternion.hpp>
 
+#define TOLLERANCE (1e-5)
 
-TEST(Quaternion, ClassInitialisation) {
+TEST(Quaternion, DefaultClassInitialisation) {
   
 	yadq::quaternion<double> q;
 
@@ -11,5 +12,33 @@ TEST(Quaternion, ClassInitialisation) {
     EXPECT_TRUE(q.x() == 0);
     EXPECT_TRUE(q.y() == 0);
     EXPECT_TRUE(q.z() == 0);
+    
+}
+
+
+TEST(Quaternion, ClassInitialisation) {
+  
+	yadq::quaternion<double> q(1, 2, 3, 4);
+
+    
+    EXPECT_TRUE(q.w() == 1);
+    EXPECT_TRUE(q.x() == 2);
+    EXPECT_TRUE(q.y() == 3);
+    EXPECT_TRUE(q.z() == 4);
+    
+}
+
+
+TEST(Quaternion, Normalisation) {
+  
+	yadq::quaternion<double> q(1, 2, 3, 4);
+
+    q.normalise();
+
+    
+    EXPECT_NEAR(q.w(), (1.0 / 5.47722557505), TOLLERANCE);
+    EXPECT_NEAR(q.x(), (2.0 / 5.47722557505), TOLLERANCE);
+    EXPECT_NEAR(q.y(), (3.0 / 5.47722557505), TOLLERANCE);
+    EXPECT_NEAR(q.z(), (4.0 / 5.47722557505), TOLLERANCE);
     
 }
