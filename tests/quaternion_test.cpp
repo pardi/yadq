745ea91b349fdca23d10961945f714e3c05ab48e
Tommaso Pardi <tommaso.pardi@gmail.com>
2023-07-11 13:29:01 +0000

    feat(test): created test for default contructor of a quaterion class

--- tests/quaternion_test.cpp
+++ tests/quaternion_test.cpp
@@ -2,11 +2,14 @@
 #include <quaternion.hpp>
 
 
-// Demonstrate some basic assertions.
 TEST(Quaternion, ClassInitialisation) {
   
 	yadq::quaternion<double> q;
 
-    EXPECT_FALSE(false);
-
+    
+    EXPECT_TRUE(q.w() == 1);
+    EXPECT_TRUE(q.x() == 0);
+    EXPECT_TRUE(q.y() == 0);
+    EXPECT_TRUE(q.z() == 0);
+    
 }
